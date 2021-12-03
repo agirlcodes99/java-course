f2f8d5081080dd4efbbba289b2af9cb7ad1030ae
agirlcodes99 <74267101+agirlcodes99@users.noreply.github.com>
2021-12-03 14:08:31 -0600

    Update custom_order.java

--- custom_order.java
+++ custom_order.java
@@ -7,29 +7,44 @@ public class custom_order {
     // STEP 1 PRINTING HELLO WORLD TO CONSOLE
     public static void main(String[] args) {
         
-      System.out.println("Hello World!"); // print Hello World to console
+      //System.out.println("Hello World!");
         
     // TEST CODE
     
     // STEP 2 CREATE A SCANNER OBEJCT, DECLARE VARAIBLES, & PRINT STATEMENTS
-	  
-
-	 
+	    Scanner keyboard = new Scanner (System.in);
+	    String firstName;
+	    String itemOrder;
+	    String frostingType;
+	    String fillingType;
+	    String toppings;
+	    String input;
+	    
+	    double cost = 15.00;
+	    final double TAX_RATE = .08;
+	    double tax;
     // Introduce shop and prompt user to input first name
-      
+	    System.out.println("Welcome to Java's Cake & Cupcake Shop!");
+	    System.out.println("We make custom cakes with our secret cake batter!");
 
 	  
 	  
     // TEST CODE
     
     // STEP 3 INPUT YOUR NAME AND PREPARE TO VIEW MENU
-   
+	    System.out.print("What is your first name? ");
+	    firstName = keyboard.nextLine();
+	    
+	    System.out.print(firstNAme + ", please see our MENU below: ");
+	    System.out.println("\n");
 	  
       
     // TEST CODE     
     
     // STEP 4 DISPLAY MENU
-
+	    System.out.println("______________________________________");
+	    System.out.println("     MENU     QUANTITY  BASE COST  ");
+	    System.out.println("______________________________________");
 	
       
 
